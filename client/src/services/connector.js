4bf350acc64b538723259da83a7e67e32a3dcb1e
simbaAshish <ashish.mahto@simbaquartz.com>
2022-12-27 12:34:31 +0530

    connector url fixed

--- client/src/services/connector.js
+++ client/src/services/connector.js
@@ -1,6 +1,6 @@
 import openSocket from "socket.io-client";
 
-const baseUrl = "http://localhost:5002/api" || "http://64.227.140.192/api";
+const baseUrl = "http://64.227.140.192/api";
 
 export const connect = async (url, method, body, token) => {
   const response = await fetch(`${baseUrl}/${url}`, {
@@ -20,6 +20,4 @@ export const connect = async (url, method, body, token) => {
   return response.json();
 };
 
-export const socket = openSocket(
-  "http://localhost:5001" || "http://64.227.140.192:5000"
-);
+export const socket = openSocket("http://64.227.140.192:5000");
